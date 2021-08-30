2ca1a6acb4e725a6038b54167811682a0d292afe
mbcordeiro <matheusdebarroscordeiro@gmail.com>
2021-08-30 00:08:33 -0300

    Car dto

---
+++ src/main/java/com/matheuscordeiro/kafkaconsumer/consumerapi/dtos/CarDTO.java
@@ -0,0 +1,14 @@
+package com.matheuscordeiro.kafkaconsumer.consumerapi.dtos;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class CarDTO {
+
+    private String id;
+    private String model;
+    private String color;
+
+}
